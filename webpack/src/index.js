a39d30dd49e5541292ae29b6d1b81a0320bc53f5
lixiaofeng <1130034920@qq.com>
2020-04-06 10:42:58 +0800

    [2020/04/06] 管理输出

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,19 +1,17 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from './data.xml';
+import printMe from "./print.js";
+
 function component() {
   var element = document.createElement("div");
+  var btn = document.createElement("button");
 
-  // lodash 是由当前 script 脚本 import 导入进来的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-  // 将图像添加到我们已经存在的 div 中。
-  var myIcon = new Image();
-  myIcon.src = Icon;
 
-  element.appendChild(myIcon);
-  console.log(Data);
+  btn.innerHTML = "点击这里，然后查看 console！";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
