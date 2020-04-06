964252c0b0b75b7f9769ab98bb5e59daa13b3a8a
lixiaofeng <1130034920@qq.com>
2020-04-06 17:15:31 +0800

    [2020/04/06] tree shaking

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,13 @@
-import _ from "lodash";
+import { cube } from "./math.js";
 
 function component() {
-  let element = document.createElement("div");
+  var element = document.createElement("pre");
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  // lodash 是由当前 script 脚本 import 进来的
+
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
 
   return element;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,12 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  // mode: "development",
+  mode: 'production',
+  optimization: {
+    usedExports: true,
+  },
 };
