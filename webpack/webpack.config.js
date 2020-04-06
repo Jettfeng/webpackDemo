de62059757f82a643e9e34979bd5a0af6dca5d35
lixiaofeng <1130034920@qq.com>
2020-04-06 11:13:46 +0800

    [2020/04/06] 使用 webpack-dev-server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,9 +3,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   output: {
     filename: "[name].bundle.js",
@@ -15,7 +20,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "管理输出",
-    })
+    }),
   ],
   module: {
     rules: [
